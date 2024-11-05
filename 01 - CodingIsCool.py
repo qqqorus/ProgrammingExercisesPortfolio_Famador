825585c15a00ec07de790a45d4b11e9739949f59
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-05 13:10:50 +0400

    Create 01 - CodingIsCool.py
    01 - Coding is Cool
    - Assigning variables and concatenating strings.

---
+++ 01 - CodingIsCool.py
@@ -0,0 +1,9 @@
+# Coding is Cool
+
+# assigning variables
+word1 = "Coding "
+word2 = "is "
+word3 = "Cool"
+
+# using string concatenation to combine the variables and print the phrase
+print(word1 + word2 + word3)
