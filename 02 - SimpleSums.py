ec2c0d6a81c5b33bb46bb6d7b4ec6125e41864bf
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-08 10:07:03 +0400

    Create 02 - SimpleSums.py
    02 - Simple Sums
    - Declaring variables, adding variables, and printing a variable

---
+++ 02 - SimpleSums.py
@@ -0,0 +1,11 @@
+# Simple Sums
+
+# declares a variable and initializing it with the integer value `8`. 
+int1 = 8
+# declares a second variable and initializing it with the integer value `10`.
+int2 = 10
+# declares a third variable that stores the sum of first two numbers.
+sum = int1 + int2
+
+# prints the value of the sum to the console.
+print(sum)
