71d4908908f19a9e7a76cdd94874b24f9f5e051b
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-15 21:15:09 +0400

    Primitive Quiz
    04 - Primitive Quiz
    - Taking input from the user and matching it with the correct variable while ignoring capitalization of the variable and input. Using if-else function to determine if the answer and input matches.

---
+++ 04 - PrimitiveQuiz.py
@@ -0,0 +1,107 @@
+# Primitive Quiz
+
+# write a program that asks the user "What is the capital of France?" and waits for their response.
+# france
+user_answer_france = str(input("What is the capital of France? "))
+correct_answer_france = "Paris"
+
+# if the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
+if user_answer_france.lower() == correct_answer_france.lower(): # .lower() ignores capitalization
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Paris.")
+# if the answer is incorrect, the program should print a message saying the answer is wrong. 
+
+
+
+# Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each question.
+
+# greece
+user_answer_greece = str(input("What is the capital of Greece? "))
+correct_answer_greece = "Athens"
+
+if user_answer_greece.lower() == correct_answer_greece.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Athens.")
+
+# united kingdom
+user_answer_uk = str(input("What is the capital of the United Kingdom? "))
+correct_answer_uk = "London"
+
+if user_answer_uk.lower() == correct_answer_uk.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is London.")
+
+# germany
+user_answer_germany = str(input("What is the capital of Germany? "))
+correct_answer_germany = "Berlin"
+
+if user_answer_germany.lower() == correct_answer_germany.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Berlin.")
+
+# russia
+user_answer_russia = str(input("What is the capital of Russia? "))
+correct_answer_russia = "Moscow"
+
+if user_answer_russia.lower() == correct_answer_russia.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Moscow.")
+
+# netherlands
+user_answer_netherlands = str(input("What is the capital of the Netherlands? "))
+correct_answer_netherlands = "Amsterdam"
+
+if user_answer_netherlands.lower() == correct_answer_netherlands.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Amsterdam.")
+
+# monaco
+user_answer_monaco = str(input("What is the capital of Monaco? "))
+correct_answer_monaco = "Monaco"
+
+if user_answer_monaco.lower() == correct_answer_monaco.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Monaco.")
+
+# belarus
+user_answer_belarus = str(input("What is the capital of Belarus? "))
+correct_answer_belarus = "Minsk"
+
+if user_answer_belarus.lower() == correct_answer_belarus.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Minsk.")
+
+# spain
+user_answer_spain = str(input("What is the capital of Spain? "))
+correct_answer_spain = "Madrid"
+
+if user_answer_spain.lower() == correct_answer_spain.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Madrid.")
+
+# poland
+user_answer_poland = str(input("What is the capital of Poland? "))
+correct_answer_poland = "Warsaw"
+
+if user_answer_poland.lower() == correct_answer_poland.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Warsaw.")
+
+# denmark
+user_answer_denmark = str(input("What is the capital of Denmark? "))
+correct_answer_denmark = "Copenhagen"
+
+if user_answer_denmark.lower() == correct_answer_denmark.lower():
+    print("You got it correct!")
+else:
+    print("You are wrong. The correct answer is Copenhagen.")
