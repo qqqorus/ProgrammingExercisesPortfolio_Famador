b30d632e98a07dca67ff96461348dfb60d478437
Quishia Desiree Famador <166199318+qqqorus@users.noreply.github.com>
2024-11-21 17:45:10 +0400

    Hello
    09 - Hello
    - Creating a hello() function to make a code to greet the user hello, and calling the hello() function inside the main() function.

---
+++ 09 - Hello.py
@@ -0,0 +1,12 @@
+# Hello
+
+# Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+
+def hello():
+    print("Hello, Quishia!")  # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello()  # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
